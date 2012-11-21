cb653280fe1d91948aaeb7a091170352564ac44d
Benjamin Fleischer <github@benjaminfleischer.com>
2012-11-21 11:48:50 -0600

    BF annotating solution to fizzbuzz so no one gets the wrong idea

--- katas/fizzbuzz.rb
+++ katas/fizzbuzz.rb
@@ -5,6 +5,7 @@ http://codingdojo.org/cgi-bin/wiki.pl?KataFizzBuzz
 Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz?".
 =end
 
+# basic, crappy solution
 (1..100).each do |number|
   just_the_number = true
   if number % 3 == 0
